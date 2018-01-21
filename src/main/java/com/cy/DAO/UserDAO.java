ec1e8b797f3db18a0dc667b50322391251f0ff5a
chenyu23 <!Cy19930923c>
2018-01-21 09:25:08 +0800

    commit8

--- src/main/java/com/cy/DAO/UserDAO.java
+++ src/main/java/com/cy/DAO/UserDAO.java
@@ -46,6 +46,10 @@ public class UserDAO extends JdbcDaoSupport {
 		return userlist;
 	}
 	
+	public void commit8(){
+		System.out.println("commit8");
+	}
+	
 	public void addUser(User user){
 		String sql="insert into user(name,tel) values(?,?)";
 		getJdbcTemplate().update(sql, new Object[]{user.getName(),user.getTel()});
@@ -85,4 +89,8 @@ public class UserDAO extends JdbcDaoSupport {
 		});
 	}
 	
+	
+	public static void main(String[] args) {
+		System.out.println("commit2");
+	}
 }
